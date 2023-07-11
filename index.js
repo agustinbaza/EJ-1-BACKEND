cc46578a92b6522d2da7318d284c2a97e369ba44
Agustin Baza <agustinbaza10@gmail.com>
2023-07-11 17:30:17 -0300

    configure las rutas y las controle con postman

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 import express from 'express';
+import cors from 'cors'
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
 
-console.log('Esto es una prueba del backend version 4')
+dotenv.config();
 
+const app = express();
+
+app.set('PORT', process.env.PORT || 4001);
+
+app.listen(app.get('PORT'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('PORT'))
+});
+
+//middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true})); 
+app.use(cors()); 
+app.use(morgan('dev')); 
+
+// rutas
+//  http://localhost:4001/prueba
+app.get('/prueba', (req, res )=>{
+    res.send('Esta es una prueba de mi ruta GET')
+})
