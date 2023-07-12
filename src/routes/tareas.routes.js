6e574cadb3800204dce2252fa464d4842a00e409
Agustin Baza <agustinbaza10@gmail.com>
2023-07-11 23:43:25 -0300

    termine las validaciones

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,16 +1,35 @@
 import { Router } from "express";
-import { borrarTarea, controladorPrueba, crearTarea, editarTarea, obtenerListaTareas, obtenerTarea } from "../controllers/tareas.controllers";
+import {
+  borrarTarea,
+  controladorPrueba,
+  crearTarea,
+  editarTarea,
+  obtenerListaTareas,
+  obtenerTarea,
+} from "../controllers/tareas.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
 router.route("/prueba").get(controladorPrueba);
-router.route("/tareas").post(crearTarea).get(obtenerListaTareas);
-router.route("/tareas/:id").get(obtenerTarea).delete(borrarTarea).put(editarTarea);
+router
+  .route("/tareas")
+  .post(
+    [
+      check("nombreTarea")
+        .notEmpty()
+        .withMessage("El nombre de la tarea es obligatorio")
+        .isLength({min:2, max:40})
+        .withMessage('El nombre de la tarea debe contener entre 2 y 100 caracteres')
+    ],
+    crearTarea
+  )
+  .get(obtenerListaTareas);
+router
+  .route("/tareas/:id")
+  .get(obtenerTarea)
+  .delete(borrarTarea)
+  .put(editarTarea);
 
 export default router;
-
-
-// app.get('/prueba', (req, res )=>{
-//     res.send('Esta es una prueba de mi ruta GET')
-// })
 
